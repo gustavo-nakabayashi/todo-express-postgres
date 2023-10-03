9fbc1d93b6b91b5db10e477c0e1a9aa6891721db
Gustavo Nakabayashi <gustavo@disco-tec.com>
2023-10-03 14:58:49 -0300

    oth: run eslint

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,51 @@
-import express, { NextFunction, Request, Response } from "express";
-import dotenv from "dotenv";
-import { Pool } from "pg";
-import cors from "cors";
+import express, { NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+import cors from 'cors';
 
 dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-})
+});
 
-console.log(process.env.DATABASE_URL)
+console.log(process.env.DATABASE_URL);
 
 const connectToDB = async () => {
   try {
     await pool.connect();
-    console.log('Connected to DB')
-
+    console.log('Connected to DB');
   } catch (err) {
-    console.log('Error connecting to DB', err)
+    console.log('Error connecting to DB', err);
   }
-}
+};
 
 connectToDB();
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.post("/todos", async (req, res) => {
+app.post('/todos', async (req, res) => {
   try {
-    const { description } = req.body
-    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description])
-    res.json(newTodo.rows[0])
+    const { description } = req.body;
+    const newTodo = await pool.query('INSERT INTO todo (description) VALUES($1) RETURNING *', [description]);
+    res.json(newTodo.rows[0]);
   } catch (err) {
     if (err) {
-      console.error(err)
-      res.status(500).send(err)
+      console.error(err);
+      res.status(500).send(err);
     } else {
-      console.log('Unkown error')
+      console.log('Unkown error');
     }
   }
-})
+});
 
-app.get("/test", (req: Request, res: Response, next: NextFunction) => {
-  res.send("hi");
-})
+app.get('/test', (req: Request, res: Response, next: NextFunction) => {
+  res.send('hi');
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`server is running at ${process.env.PORT}`)
-})
+  console.log(`server is running at ${process.env.PORT}`);
+});
--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,15 @@
 const axios = require('axios')
 
+const axiosConfig = {
+  baseURL: `http://127.0.0.1:${apiConnection.port}`,
+  validateStatus: () => true, //Don't throw HTTP exceptions. Delegate to the tests to decide which error is acceptable
+};
+axiosAPIClient = axios.create(axiosConfig);
+
 describe('/POST /todos', () => {
   test('Should return 200', async () => {
     // Arrange
+
 
     // Act
     const response = await axios.post("http://localhost:5000/todos", { description: 'teste' })
