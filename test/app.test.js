fe2fecc7ed74839985b6bdae5ba2ec0c886cc7f0
Gustavo Nakabayashi <gustavo@disco-tec.com>
2023-10-05 12:42:36 -0300

    oth: reorganize test names

--- test/app.test.js
+++ test/app.test.js
@@ -9,64 +9,70 @@ const axiosConfig = {
 };
 const axiosAPIClient = axios.create(axiosConfig);
 
-describe('/POST /todos', () => {
-  test('Should return 200', async () => {
-    // Arrange
-    // Act
-    const response = await axiosAPIClient.post('/todos', { description: 'teste' });
-    // Assert
-    const { status, data } = response;
-    expect(status).toEqual(200);
-    expect(data).toHaveProperty('description', 'teste');
+describe('/todos', () => {
+  describe('POST', () => {
+    test('Should return 200', async () => {
+      // Arrange
+      const todoToAdd = { description: 'teste' };
+      // Act
+      const {
+        data: { todo_id: addedOrderId },
+      } = await axiosAPIClient.post('/todos', todoToAdd);
+      // Assert
+      const { data, status } = await axiosAPIClient.get(`/todos/${addedOrderId}`);
+      expect({ data, status }).toMatchObject({ data: { todo_id: addedOrderId, description: 'teste' }, status: 200 });
+    });
   });
-});
 
-describe('/GET /todos', () => {
-  test('Should return added todo', async () => {
-    // Arrange
-    const uniqueDescription = new Date().getTime().toString();
-    const todoToAdd = { description: uniqueDescription };
-    await axiosAPIClient.post('/todos', todoToAdd);
-    // Act
-    const response = await axiosAPIClient.get('/todos');
-    // Assert
-    const { status, data } = response;
-    const addedTodo = data.rows.filter((todo) => todo.description == uniqueDescription)[0];
-    expect(status).toEqual(200);
-    expect(addedTodo).toHaveProperty('description', uniqueDescription);
+  describe('GET', () => {
+    test('Should return added todo', async () => {
+      // Arrange
+      const uniqueDescription = new Date().getTime().toString();
+      const todoToAdd = { description: uniqueDescription };
+      await axiosAPIClient.post('/todos', todoToAdd);
+      // Act
+      const response = await axiosAPIClient.get('/todos');
+      // Assert
+      const { status, data } = response;
+      const addedTodo = data.rows.filter((todo) => todo.description == uniqueDescription)[0];
+      expect(status).toEqual(200);
+      expect(addedTodo).toHaveProperty('description', uniqueDescription);
+    });
   });
-});
 
-describe('/GET /todos/:id', () => {
-  test('Should return a todo by id', async () => {
-    // Arrange
-    const uniqueDescription = new Date().getTime().toString();
-    const todoToAdd = { description: uniqueDescription };
-    const addedTodo = await axiosAPIClient.post('/todos', todoToAdd);
-    const { todo_id } = addedTodo.data;
-    // Act
-    const response = await axiosAPIClient.get(`/todos/${todo_id}`);
-    // Assert
-    const { status, data } = response;
-    expect(status).toEqual(200);
-    expect(data).toHaveProperty('todo_id', todo_id);
-  });
-});
+  describe('/:id', () => {
+    describe('GET', () => {
+      test('Should return a todo by id', async () => {
+        // Arrange
+        const uniqueDescription = new Date().getTime().toString();
+        const todoToAdd = { description: uniqueDescription };
+        const addedTodo = await axiosAPIClient.post('/todos', todoToAdd);
+        const { todo_id } = addedTodo.data;
+        // Act
+        const response = await axiosAPIClient.get(`/todos/${todo_id}`);
+        // Assert
+        const { status, data } = response;
+        expect(status).toEqual(200);
+        expect(data).toHaveProperty('todo_id', todo_id);
+      });
+    });
 
-describe('/PUT /todos/:id', () => {
-  test('Should return update a todo by id', async () => {
-    // Arrange
-    const uniqueDescription = new Date().getTime().toString();
-    const todoToAdd = { description: uniqueDescription };
-    const addedTodo = await axiosAPIClient.post('/todos', todoToAdd);
-    const { todo_id } = addedTodo.data;
-    // Act
-    const updatedResponse = await axiosAPIClient.put(`/todos/${todo_id}`, { description: 'updated' });
-    const response = await axiosAPIClient.get(`/todos/${todo_id}`);
-    // Assert
-    const { data } = response;
-    const { status } = updatedResponse;
-    expect(status).toEqual(200);
-    expect(data).toHaveProperty('description', 'updated');
+    describe('PUT', () => {
+      test('Should return update a todo by id', async () => {
+        // Arrange
+        const uniqueDescription = new Date().getTime().toString();
+        const todoToAdd = { description: uniqueDescription };
+        const addedTodo = await axiosAPIClient.post('/todos', todoToAdd);
+        const { todo_id } = addedTodo.data;
+        // Act
+        const updatedResponse = await axiosAPIClient.put(`/todos/${todo_id}`, { description: 'updated' });
+        const response = await axiosAPIClient.get(`/todos/${todo_id}`);
+        // Assert
+        const { data } = response;
+        const { status } = updatedResponse;
+        expect(status).toEqual(200);
+        expect(data).toHaveProperty('description', 'updated');
+      });
+    });
   });
 });
